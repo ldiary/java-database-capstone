9626d5144085ea628dcd042b717e1e36b72ce53c
Ernesto Luzon <edluzonjr@gmail.com>
2025-07-27 21:51:43 -0400

    started templating

--- app/src/main/resources/static/js/services/index.js
+++ app/src/main/resources/static/js/services/index.js
@@ -1,3 +1,25 @@
+import { openModal } from '../components/modals.js'
+import { API_BASE_URL } from '../config/config.js'
+
+
+const ADMIN_API = API_BASE_URL + '/admin';
+const DOCTOR_API = API_BASE_URL + '/doctor/login'
+
+window.onload = function () {
+    const adminBtn = document.getElementById('adminLogin');
+    const doctorBtn = document.getElementById('doctorLogin');
+    if (adminBtn) {
+        adminBtn.addEventListener('click', () => {
+            openModal('adminLogin');
+        });
+    }
+    else if (doctorBtn) {
+        doctorBtn.addEventListener('click', () => {
+            openModal('doctorBtn');
+        });
+}
+
+
 /*
   Import the openModal function to handle showing login popups/modals
   Import the base API URL from the config file
